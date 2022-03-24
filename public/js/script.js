d8b6115117afe0aed1fd2b31a67bc6aa496ef3ae
wahil chettouf <wahilchettouf@gmail.com>
2022-03-24 07:44:33 +0100

    Ajouter des fichiers de base pour la première version de ce projet -1.0

---
+++ public/js/script.js
@@ -0,0 +1,28 @@
+
+var num = 2;
+function menuApp() {
+    var div_tirer = document.getElementById('div-tirer');
+    var menu_app = document.getElementById('menu-app');
+    
+    if (num%2==0) {
+        menu_app.style.display="flex";
+
+    } else {
+        menu_app.style.display="none";
+    };
+    num++
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
